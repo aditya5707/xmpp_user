0069f6c2d69459ff653c65adc2f8c649f310e685
aditya <adityagarg.work@gmail.com>
2012-06-20 21:35:20 +0530

    updated gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XmppUser::Application.config.secret_token = '1dcacdc30ed2920e62cd4fa249ad8e27b6834ad5d0b29ca871c96fe2ebb3f01f316d152cc318d9fbffb5d362059b5a91fc31b7165a8def1342b8c80ad7db8236'
